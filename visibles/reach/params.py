0ed4441a106a575443acedf764b544fd961ed9fe
Jessica Pascadlo <jlp4td@virginia.edu>
2023-10-04 12:12:05 -0400

    Added more usable framework for selecting sub or whole detector parameters

--- visibles/reach/params.py
+++ visibles/reach/params.py
@@ -1,30 +1,38 @@
 outfile = "output"
 
 ebeam = 4. #GeV
-#e_zmin = 50. #cm
-#e_zmax = 70. #cm
+zmin = 50. #cm
+zmax = 500. #cm
+e_zmin = 50. #cm
+e_zmax = 70. #cm
 h_zmin = 70. #cm
 h_zmax = 500. #cm
 eot = 4.e14
-#ecal_background = 0
+ecal_background = 5
 hcal_background = 10
+combined_background = 15
 
 NepsBins = 1000
-#epsmin = -7 #logeps
-#epsmax = -2 #logeps
-epsmin = -11
-epsmax = -4
+epsmin = -7 #logeps
+epsmax = -2 #logeps
 
 nMass = 1000
 massmin = -2 #logmass GeV
 massmax = 0 #logmass GeV
 
 csvoutput = True      #default to True
-plotoutput = False    #default to True
-eps2 = True           #default to False, sets whether or not we want eps^2
+plotoutput = True    #default to True
+eps2 = False           #default to False, sets whether or not we want eps^2
 eatvis = False        #default to False
 
+#set only one of these to True at a time
+Ecal = False          #default to False, select Ecal only parameters
+Hcal = False          #default to False, select Hcal only parameters
+Combined = False      #default to False, combine ecal/hcal parameters (distance and background amounts)
+
 eff_const = 0.5
+
+#things that may be used in the future
 subsys_eff = False
 mass_values = [0.01, 0.05] #GeV
 ecal_eff = [0.5, 0.5] #GeV
--- visibles/reach/reach.py
+++ visibles/reach/reach.py
@@ -19,7 +19,7 @@ def load_module(file_name, module_name):
     spec.loader.exec_module(module)
     return module
 
-options, remainder = getopt.gnu_getopt(sys.argv[1:], 'h')
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'hcba:')
 # Parse the command line arguments
 for opt, arg in options:
     if opt=='-h':
@@ -43,23 +43,69 @@ f.close()
 my_module = load_module(paramfile, "params")
 from params import *
 
-if(eps2):
-    outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}hcalbkg_{6:.0f}eff_eps2".format(outfile, eot, ebeam, h_zmin, h_zmax, hcal_background, eff_const*100)
-else:
-    outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}hcalbkg_{6:.0f}eff".format(outfile, eot, ebeam, h_zmin, h_zmax, hcal_background, eff_const*100)
-
-minSignal = rc.MinSignal(hcal_background)
-
-if(plotoutput):
-    outplot = outbasename
-    p.MakePlots(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, ebeam, eot, e_zmin, e_zmax, eff_const, outplot, eatvis = eatvis)
-
-if(csvoutput):
-    _, _, detectable = p.MakeHistos(massmin, massmax, nMass, epsmin, epsmax, NepsBins, ebeam, eot, h_zmin, h_zmax, eff_const, eatvis = eatvis)
-    outcsv = outbasename + ".csv"
+if (Ecal):
     if(eps2):
-        import ContourCSVeps2 as c
-        c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+        outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}ecalbkg_{6:.0f}eff_eps2".format(outfile, eot, ebeam, e_zmin, e_zmax, ecal_background, eff_const*100)
     else:
-        import ContourCSV as c
-        c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+        outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}ecalbkg_{6:.0f}eff".format(outfile, eot, ebeam, e_zmin, e_zmax, ecal_background, eff_const*100)
+
+        minSignal = rc.MinSignal(ecal_background)
+
+    if(plotoutput):
+        outplot = outbasename
+        p.MakePlots(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, ebeam, eot, e_zmin, e_zmax, eff_const, outplot, eatvis = eatvis)
+
+    if(csvoutput):
+        _, _, detectable = p.MakeHistos(massmin, massmax, nMass, epsmin, epsmax, NepsBins, ebeam, eot, e_zmin, e_zmax, eff_const, eatvis = eatvis)
+        outcsv = outbasename + ".csv"
+        if(eps2):
+            import ContourCSVeps2 as c
+            c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+        else:
+            import ContourCSV as c
+            c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+
+if (Hcal):
+    if(eps2):
+        outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}hcalbkg_{6:.0f}eff_eps2".format(outfile, eot, ebeam, h_zmin, h_zmax, hcal_background, eff_const*100)
+    else:
+        outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}hcalbkg_{6:.0f}eff".format(outfile, eot, ebeam, h_zmin, h_zmax, hcal_background, eff_const*100)
+
+        minSignal = rc.MinSignal(hcal_background)
+
+    if(plotoutput):
+        outplot = outbasename
+        p.MakePlots(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, ebeam, eot, h_zmin, h_zmax, eff_const, outplot, eatvis = eatvis)
+
+    if(csvoutput):
+        _, _, detectable = p.MakeHistos(massmin, massmax, nMass, epsmin, epsmax, NepsBins, ebeam, eot, h_zmin, h_zmax, eff_const, eatvis = eatvis)
+        outcsv = outbasename + ".csv"
+        if(eps2):
+            import ContourCSVeps2 as c
+            c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+        else:
+            import ContourCSV as c
+            c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+
+
+if (Combined):
+    if(eps2):
+        outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}combbkg_{6:.0f}eff_eps2".format(outfile, eot, ebeam, zmin, zmax, combined_background, eff_const*100)
+    else:
+        outbasename = "{0}_{1:.0e}eot_{2:.0f}gev_{3:.0f}-{4:.0f}cm_{5:.0f}combbkg_{6:.0f}eff".format(outfile, eot, ebeam, zmin, zmax, combined_background, eff_const*100)
+
+        minSignal = rc.MinSignal(combined_background)
+
+    if(plotoutput):
+        outplot = outbasename
+        p.MakePlots(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, ebeam, eot, zmin, zmax, eff_const, outplot, eatvis = eatvis)
+
+    if(csvoutput):
+        _, _, detectable = p.MakeHistos(massmin, massmax, nMass, epsmin, epsmax, NepsBins, ebeam, eot, zmin, zmax, eff_const, eatvis = eatvis)
+        outcsv = outbasename + ".csv"
+        if(eps2):
+            import ContourCSVeps2 as c
+            c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
+        else:
+            import ContourCSV as c
+            c.OutputCSV(massmin, massmax, nMass, epsmin, epsmax, NepsBins, minSignal, detectable, outcsv)
